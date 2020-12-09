8208a0b9ba2b9ce2d2bfb68ff710b1cf52c6ad40
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-12-09 22:16:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import authorsReducer from './reducers/authorsReducer';
-import booksReducer from './reducers/booksReducer
+import booksReducer from './reducers/booksReducer;
 
 
 const rootReducer = combineReducers({
